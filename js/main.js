f1de8fd327d000b7af781a250ada5dc1f698d14a
poncianodev <lucasbponciano@hotmail.com>
2024-07-22 22:00:59 -0300

    Foi corrigo o problema do menu mobile não estar fechando

--- js/main.js
+++ js/main.js
@@ -2,7 +2,14 @@
 
 const mobileMenuContainer = document.querySelector(".mobile-menu-container");
 const headerMenu = document.querySelector(".header-menu");
+const menuItems = document.querySelectorAll(".header-menu li");
 
 mobileMenuContainer.addEventListener("click", () => {
   headerMenu.classList.toggle("active");
+});
+
+menuItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    headerMenu.classList.remove("active");
+  });
 });
